90655b44e57deb09fa088ec88a469b239a339722
OmarZaher16008288 <omar.zaher@student.giu-uni.de>
2025-11-11 09:02:37 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -2,9 +2,21 @@
 #define INSTRUCTOR_H
 
 #include "Person.h"
+#include <string>
+
 using namespace std;
 //#write Instructor class here
+class Insructor: public Person{
 
+private:
+
+string departement;
+int experiences;
+
+public :
+Insructor(string n,int i,string d,int e);
+void display();
+};
 
 
 
